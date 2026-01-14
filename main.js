cc881be95c3cb7c57b20106684dd148611a8fff4
nightZQ <pandazhen11@gmail.com>
2026-01-15 02:31:47 +0800

    feat: rough patient panel

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-	const navbar = document.getElementById('navbar');
-	const hamburger = document.getElementById('hamburger-nav');
-	const iconMenu = document.getElementById('nav-menu');
-	const iconClose = document.getElementById('nav-close');
-	hamburger.addEventListener('click', () => {
-		const isNavHidden = navbar.classList.contains('md:hidden');;
-		navbar.classList.toggle('md:hidden');
-		navbar.classList.toggle('md:flex');
-		iconMenu.classList.toggle('hidden', isNavHidden);
-		iconClose.classList.toggle('hidden', !isNavHidden);
-	});
+// Navbar tablet view: toggle on/off
+const navbar = document.getElementById('navbar');
+const hamburger = document.getElementById('hamburger-nav');
+const iconMenu = document.getElementById('nav-menu');
+const iconClose = document.getElementById('nav-close');
+hamburger.addEventListener('click', () => {
+	const isNavHidden = navbar.classList.contains('md:hidden');;
+	navbar.classList.toggle('md:hidden');
+	navbar.classList.toggle('md:flex');
+	iconMenu.classList.toggle('hidden', isNavHidden);
+	iconClose.classList.toggle('hidden', !isNavHidden);
+});
+
+// Patient panel
